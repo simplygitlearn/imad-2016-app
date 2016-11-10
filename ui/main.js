5516112e68a72e7cf0cf41c0af431ff19986a2ad
simplygitlearn <sahaparamjit2015@gmail.com>
2016-11-11 01:36:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,7 @@
 var button = document.getElementById('counter');
 counter = 0;
 button.onclick = function() {
+    alert("Hello! I am an alert box!!");
     counter = 1;
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
