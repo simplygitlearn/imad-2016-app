42a81ef23088115a0e720d1d2832fa7a5ab63473
simplygitlearn <sahaparamjit2015@gmail.com>
2016-11-11 01:32:25 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,5 +3,5 @@ counter = 0;
 button.onclick = function() {
     counter = counter + 1;
     var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
+    span.innerHTML = counter;
 };
