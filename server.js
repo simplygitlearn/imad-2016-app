81fcdd88813c3563e830e76b41ea237ce1123ffd
simplygitlearn <sahaparamjit2015@gmail.com>
2016-11-11 02:08:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,6 +3,26 @@ var morgan = require('morgan');
 var path = require('path');
 
 var app = express();
+var pg = require('pg');
+
+var dbconfig = {
+    host: 'db.imad.hasura-app.io',
+    user: 'simplygitlearn',
+    password: process.env.DB_PASSWORD,
+    port: 5432,
+    database: 'simplygitlearn'
+};
+var pool = new pg.Pool(config);
+app.get('/test-db', function(req, res){
+   pool.query('SELECT * FROM test', function(err, result){
+      if(err){
+          res.status(500).send(err, toString());
+      } else {
+          res.send(JSON.stringify(result));
+      }
+   }); 
+});
+
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
