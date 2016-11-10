ea664463907eec1665370cc77fc4e765deae3409
simplygitlearn <sahaparamjit2015@gmail.com>
2016-11-11 02:12:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var dbconfig = {
     port: 5432,
     database: 'simplygitlearn'
 };
-var pool = new Pool(config);
+var pool = new Pool(dbconfig);
 app.get('/test-db', function(req, res){
    pool.query('SELECT * FROM test', function(err, result){
       if(err){
